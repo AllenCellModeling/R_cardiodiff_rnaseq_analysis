db81617d7aacd690ab03b0c4147e99c728cc9645
Rory Donovan-Maiye <rorydm@alleninstitute.org>
2018-01-16 11:55:24 -0800

    get notebook running

--- run_docker_jupyter.sh
+++ run_docker_jupyter.sh
@@ -4,7 +4,7 @@ PORT=${1}
 
 docker run -ti \
     -p ${PORT}:8888 \
-    -v /allen/aics/modeling/${USER}/projects:/root/projects \
-    -v /allen/aics:/root/aics \
+    -v /allen/aics/modeling/${USER}/projects:/home/jovyan/projects \
+    -v /allen/aics:/home/jovyan/aics \
     rorydm/r_rnaseq
 
